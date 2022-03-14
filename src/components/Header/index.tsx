efbbd9189d68709e31c6425f25590f4d97d95d32
Thiago Cabral <thiagocabral477@gmail.com>
2022-03-13 23:39:40 -0300

    adding faunaDb and Stripe

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import { SignInUpButton } from '../SignInUpButton'
+import styles from './styles.module.scss'
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+
+        <SignInUpButton />
+      </div>
+    </header>
+  )
+}
